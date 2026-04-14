3e7b52c3a6f88ca749d41e948537cab247364175
llacidrainll <gdgarcia1209@gmail.com>
2026-04-14 21:15:39 +0000

    finished mini02 removed any item labeled "optional"

--- week5-exercises/mini02-list-mechanics-listiterator/src/main/java/edu/norcocollege/cis18b/week5/mini02/AgendaEditor.java
+++ week5-exercises/mini02-list-mechanics-listiterator/src/main/java/edu/norcocollege/cis18b/week5/mini02/AgendaEditor.java
@@ -25,7 +25,8 @@ public class AgendaEditor {
                 iterator.set("Live Demo");
                 iterator.add("Reflection");
             }
-
+             if ("Optional".equals(step)) {
+            iterator.remove();
             // TODO: Remove any item labeled "Optional" using iterator-safe mutation.
         }
     }
